3cba2c28846ab78a17dbd776a17fb0cfb3b77153
lucas.. <lucas.joaed@gmail.com>
2025-11-26 20:20:10 -0300

    fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 
 const startServer = async () => {
   app.listen(port, () => {
-    console.log(`Servidor ATLANTIDA-API escutando em http://localhost:${port}`);
+    console.log(`Servidor ATLANTIDA-API escutando na porta ${port}`);
   });
 };
 
--- src/application.js
+++ src/application.js
@@ -21,7 +21,7 @@ const __dirname  = path.dirname(__filename);
    Middlewares base
 ========================= */
 app.use(cors({
-  origin: "https://atlantida-seven.vercel.app/",
+  origin: "https://atlantida-seven.vercel.app",
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
